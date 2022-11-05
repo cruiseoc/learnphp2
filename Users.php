d3d8f56c447eeafc55087990941cdd648f8aa23f
cruiseoc <cruise.oc@oundleschool.org.uk>
2022-11-05 12:05:56 +0000

    nfg

--- Users.php
+++ Users.php
@@ -15,8 +15,5 @@
   <input type="radio" name="role" value="Teacher"> Teacher<br>
   <input type="radio" name="role" value="Admin"> Admin<br>
   <input type="submit" value="Add User">
-  <?php
-    echo("submitted")
-    ?>
 
 </form>
