0dffe2ff0b6d3b5eb31f207f846f18ad9bf19b7d
cruiseoc <cruise.oc@oundleschool.org.uk>
2022-11-05 12:08:10 +0000

    dfd

--- Users.php
+++ Users.php
@@ -1,4 +1,4 @@
-<form action="addusers.php">
+<form action="addusers.php" method = "post">
   First name:<input type="text" name="forename"><br>
   Last name:<input type="text" name="surname"><br>
   Password:<input type="password" name="passwd"><br>
